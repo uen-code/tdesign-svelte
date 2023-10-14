4d8f6ef51b64b9b69efc180d0dd46057c534a72e
programmerHZX <641477144@qq.com>
2023-10-14 22:37:46 +0800

    fead: add web component the tdesign-component-site

--- site/src/main.js
+++ site/src/main.js
@@ -1,5 +1,15 @@
 import App from './App.svelte'
 
+// import site webComponents
+import 'tdesign-site-components';
+import 'tdesign-site-components/lib/styles/style.css';
+import 'tdesign-site-components/lib/styles/prism-theme.less';
+import 'tdesign-site-components/lib/styles/prism-theme-dark.less';
+
+// register site change
+import { registerLocaleChange } from 'tdesign-site-components';
+
+registerLocaleChange();
 
 const app = new App({
   target: document.getElementById('app'),
