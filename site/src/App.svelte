7e56e3659c5ad3848574fc27a472bc4779b1d964
programmerHZX <641477144@qq.com>
2023-10-31 08:30:04 +0800

    test breach

--- site/src/App.svelte
+++ site/src/App.svelte
@@ -18,7 +18,7 @@
   <td-header slot="header" framework="svelte">
   </td-header>
   <td-doc-aside title="Svelte for Web" bind:this={dom} on:change={change}>
-    <td-select value="1" slot="extra"></td-select>
+    <td-select value="2" slot="extra"></td-select>
   </td-doc-aside>
   <Router>
     {#each menuItems as menuItem}
